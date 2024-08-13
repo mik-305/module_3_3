46b12e69714c7af66977fd3d2fa28230c7d73e24
mik-305 <mik-305@bk.ru>
2024-08-13 16:11:56 +0300

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -1,11 +1,13 @@
 def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
-
+print('0 ------------Добавочная проверка------------')
+print_params(b = 25)
+print_params(c = [1, 2, 3])
 values_list = [33, 'AAAAAA', False]
 print_params()
 print_params(*values_list)
-print('1 ------------Промежуточная проверка---------')
+print('1 -------------Промежуточная проверка----------')
 values_dict = {'a': 77, 'b': 'FFFFF', 'c': True}
 print_params(**values_dict)
 print('2 ------------Итоговая проверка--------------')
