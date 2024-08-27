a420d3ee61e6ee1cab3e9fd94cf0dce3abaf43d3
mik-305 <mik-305@bk.ru>
2024-08-27 14:16:49 +0300

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -1,15 +1,20 @@
-def print_params(a=1, b='строка', c=True):
+def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
 
-print('0 ------------Добавочная проверка------------')
+print('----1 п.------------')
+print_params()          # Без параметров
+print_params(a = 999, c = 121)
 print_params(b = 25)
 print_params(c = [1, 2, 3])
-values_list = [33, 'AAAAAA', False]
-print_params()
+
+print('----2 п.------------')
+
+values_list = [77, 'БАСКЕТБОЛ', True] # Параметры различных типов для списка
 print_params(*values_list)
-print('1 -------------Промежуточная проверка----------')
-values_dict = {'a': 77, 'b': 'FFFFF', 'c': True}
+
+values_dict = {'a':'ЧислО','b':'Набор_букВ ','c':'ЛогикА'} # Словарь
 print_params(**values_dict)
-print('2 ------------Итоговая проверка--------------')
-values_list_2 = [888, 'КЛЯКСА']
+
+print('----3 п.------------')
+values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
